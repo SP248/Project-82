a64983e6dca8ebe7538ccb447032d58a0389aee7
SP248 <82733585+SP248@users.noreply.github.com>
2022-04-26 20:24:03 -0400

    Add files via upload

---
+++ Project 81/PostCard.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+
+
+
+render() {
+    return(
+        <View style = {styles.container}>
+            <View style = {styles.cardContainer}>
+                <View style = {styles.authorContainer}>
+                    <View style = {style.authorImageContainer}>
+                        <Image 
+                        source = {require("../assets/profile_img.png")}
+                        style = {styles.profileImage}
+                        ></Image>
+                            </View>
+                            <View style = {styles.authorNameContainer}>
+                                <Text style = {styles.authoeNameText}>{this.props.post.author}</Text>
+                                </View>
+                                </View>
+                                <Image source = {require("../assets/post.jpeg")} style = {styles.postImage}/>
+                                <View style = {styles.captionContainer}>
+                                    <Text style = {styles.captionText}>
+                                        {this.props.post.caption}
+                                        </Text>
+                                </View>
+                                <View style = {styles.actionContainer}>
+                                    <View style = {styles.likeButton}>
+                                        <Ionicons name = {"heart"} size = {RFValue(30)} color = {"white"} />
+                                        <Text style = {styles.likeText}>12k</Text>
+                                        </View>
+                                        </View>
+                                        </View>
+                                        </View>
+                
+    );
+}
